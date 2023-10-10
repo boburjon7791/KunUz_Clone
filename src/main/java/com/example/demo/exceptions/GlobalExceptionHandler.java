b7e56ff35e33607ed507697f6e43ff563092617a
Soliyev Boburjon <122117930+boburjon7791@users.noreply.github.com>
2023-10-10 20:17:49 +0500

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.nio.file.NoSuchFileException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler{
